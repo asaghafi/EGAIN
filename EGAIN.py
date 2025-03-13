b4010b4cd5b9d9ff66c41ad26548076b3cf0d7dc
Abo <abolfazl.saghafi@gmail.com>
2025-03-13 09:42:12 -0400

    Update EGAIN.py

--- EGAIN.py
+++ EGAIN.py
@@ -1,11 +1,6 @@
-#
-# GNU GENERAL PUBLIC LICENSE
-# Version 3, 29 June 2007
-#
-#  Copyright (C) 2007 Free Software Foundation, Inc.
-# Everyone is permitted to copy and distribute verbatim copies
-# of this license document, but changing it is not allowed.
-#
+#                                 Apache License
+#                           Version 2.0, January 2004
+#                        http://www.apache.org/licenses/
 
 ## Import Packages
 ##-------------------
